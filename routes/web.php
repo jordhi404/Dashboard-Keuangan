cf1aef05ca3f5c4444fa3ca50d24819306686747
jordhi404 <tejosaputro22@gmail.com>
2024-07-06 12:40:31 +0700

    Perubahan 	modified:   app/Providers/AppServiceProvider.php 	new file:   app/deleteDebugbarB4Deploy.txt 	modified:   bootstrap/providers.php 	new file:   resources/views/Keuangan/login.blade.php 	modified:   routes/web.php 	new file:   storage/debugbar/.gitignore

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BedController;
+use Barryvdh\Debugbar\Facades\Debugbar;
 
 Route::get('/', function () {
+    Debugbar::info('Test debugbar');
     return view('welcome');
 });
 
-Route::get('/index', [BedController::class, 'index'])->name('Keuangan.index');
+Route::get('/index',[BedController::class, 'index'])->name('Keuangan.index');
