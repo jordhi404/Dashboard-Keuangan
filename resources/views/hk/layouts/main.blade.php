e76277f7bad4fc88d4bfebd6bca189c64719d6b0
yusufbagussh <yusuf.herlambang27@gmail.com>
2024-08-24 11:10:20 +0700

    update icon dashboard hk

--- resources/views/hk/layouts/main.blade.php
+++ resources/views/hk/layouts/main.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="refresh" content="180">
     <title>House Keeping</title>
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('Logo_img/house_keeping_white.png') }}">
+    <link rel="icon" href="{{ asset('Logo_img/logo_rs.jpg') }}" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
