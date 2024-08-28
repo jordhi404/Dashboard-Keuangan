efaa394962b9d98e89580a1b8fc5118129d2086b
yusufbagussh <yusuf.herlambang27@gmail.com>
2024-08-28 14:36:06 +0700

    feat: authorization

--- app/Http/Controllers/RanapController.php
+++ app/Http/Controllers/RanapController.php
@@ -15,11 +15,11 @@ class RanapController extends Controller
     {
         $cacheKey = 'patientRanap';
 
-        return Cache::remember($cacheKey, 240, function() {
+        return Cache::remember($cacheKey, 240, function () {
             return DB::connection('sqlsrv')
-                -> select("
+                ->select("
                 WITH Dashboard_CTE AS (
-                    SELECT DISTINCT 
+                    SELECT DISTINCT
                         a.RegistrationNo,
                         r.ServiceUnitName,
                         a.BedCode,
@@ -27,55 +27,55 @@ class RanapController extends Controller
                         a.PatientName,
                         r.CustomerType,
                         r.ChargeClassName,
-                        RencanaPulang = 
-                            CASE 
+                        RencanaPulang =
+                            CASE
                                 WHEN cv.PlanDischargeTime IS NULL
                                     THEN CAST(cv.PlanDischargeDate AS VARCHAR) + ' ' + CAST(cv.PlanDischargeTime AS VARCHAR)
                                 ELSE CAST(cv.PlanDischargeDate AS VARCHAR) + ' ' + CAST(cv.PlanDischargeTime AS VARCHAR)
                             END,
                         CatRencanaPulang = cv.PlanDischargeNotes,
                         Keperawatan =
-                            (SELECT TOP 1 TransactionNo 
+                            (SELECT TOP 1 TransactionNo
                             FROM PatientChargesHD
-                            WHERE VisitID=cv.VisitID 
-                            AND GCTransactionStatus<>'X121^999' 
+                            WHERE VisitID=cv.VisitID
+                            AND GCTransactionStatus<>'X121^999'
                             AND GCTransactionStatus IN ('X121^001','X121^002','X121^003')
                             AND HealthcareServiceUnitID NOT IN (82,83,99,138,140,101,137)
                             ORDER BY TestOrderID ASC),
-                        TungguJangdik = 
-                            (SELECT TOP 1 TransactionNo 
+                        TungguJangdik =
+                            (SELECT TOP 1 TransactionNo
                             FROM PatientChargesHD
-                            WHERE VisitID=cv.VisitID 
-                            AND GCTransactionStatus<>'X121^999' 
+                            WHERE VisitID=cv.VisitID
+                            AND GCTransactionStatus<>'X121^999'
                             AND GCTransactionStatus IN ('X121^001','X121^002','X121^003')
                             AND HealthcareServiceUnitID IN (82,83,99,138,140)
                             ORDER BY TestOrderID ASC),
-                        TungguFarmasi = 
-                            (SELECT TOP 1 TransactionNo 
+                        TungguFarmasi =
+                            (SELECT TOP 1 TransactionNo
                             FROM PatientChargesHD
-                            WHERE VisitID=cv.VisitID 
-                            AND GCTransactionStatus<>'X121^999' 
+                            WHERE VisitID=cv.VisitID
+                            AND GCTransactionStatus<>'X121^999'
                             AND GCTransactionStatus IN ('X121^001','X121^002','X121^003')
                             AND HealthcareServiceUnitID IN (101,137)
                             ORDER BY TestOrderID ASC),
-                        RegistrationStatus = 
+                        RegistrationStatus =
                             (SELECT TOP 1 IsLockDownNEW
-                            FROM RegistrationStatusLog 
-                            WHERE RegistrationID = a.RegistrationID 
+                            FROM RegistrationStatusLog
+                            WHERE RegistrationID = a.RegistrationID
                             ORDER BY ID DESC),
                         OutStanding =
-                            (SELECT DISTINCT COUNT(GCTransactionStatus) 
-                            FROM PatientChargesHD 
-                            WHERE VisitID=cv.VisitID 
+                            (SELECT DISTINCT COUNT(GCTransactionStatus)
+                            FROM PatientChargesHD
+                            WHERE VisitID=cv.VisitID
                             AND GCTransactionStatus IN ('X121^001','X121^002','X121^003')),
-                        SelesaiBilling = 
-                            (SELECT TOP 1 PrintedDate 
-                            FROM ReportPrintLog 
-                            WHERE ReportID=7012 
-                            AND ReportParameter = CONCAT('RegistrationID = ',r.RegistrationID) 
+                        SelesaiBilling =
+                            (SELECT TOP 1 PrintedDate
+                            FROM ReportPrintLog
+                            WHERE ReportID=7012
+                            AND ReportParameter = CONCAT('RegistrationID = ',r.RegistrationID)
                             ORDER BY PrintedDate DESC),
                         Keterangan =
-                        CASE 
+                        CASE
                             WHEN sc.StandardCodeName = '' OR sc.StandardCodeName IS NULL
                                 THEN ''
                             ELSE sc.StandardCodeName
@@ -86,14 +86,14 @@ class RanapController extends Controller
                     LEFT JOIN vRegistration r ON r.RegistrationID = a.RegistrationID
                     LEFT JOIN ConsultVisit cv ON cv.VisitID = r.VisitID
                     LEFT JOIN StandardCode sc ON sc.StandardCodeID = cv.GCPlanDischargeNotesType
-                    LEFT JOIN PatientVisitNote pvn ON pvn.VisitID = cv.VisitID 
+                    LEFT JOIN PatientVisitNote pvn ON pvn.VisitID = cv.VisitID
                         AND pvn.GCNoteType IN ('X312^001', 'X312^002', 'X312^003', 'X312^004', 'X312^005', 'X312^006')
-                    WHERE a.IsDeleted = 0 
+                    WHERE a.IsDeleted = 0
                     AND a.RegistrationID IS NOT NULL
                     AND cv.PlanDischargeDate IS NOT NULL
                     AND r.GCRegistrationStatus <> 'X020^006' -- Pendaftaran Tidak DiBatalkan
                 )
-                SELECT 
+                SELECT
                     RegistrationNo,
                     ServiceUnitName,
                     BedCode,
@@ -117,87 +117,88 @@ class RanapController extends Controller
     }
 
     /* FUNCTION UNTUK MENAMPILKAN DATA DI DASHBOARD RANAP. */
-    public function showdashboardRanap() {
+    public function showdashboardRanap()
+    {
 
         // Assign ServiceUnitName berdasarkan kode_bagian pengguna yang sedang login
         $user = Auth::user();
         $serviceUnit = $this->getServiceUnit($user->kode_bagian);
 
         /* MENGAMBIL DATA PASIEN UNTUK DITAMPILKAN. */
-            // Mengambil data pasien untuk ditampilkan berdasarkan ServiceUnitName
-            if ($user->kode_bagian == 'k45') {
-                $patients = collect($this->getPatientData());
+        // Mengambil data pasien untuk ditampilkan berdasarkan ServiceUnitName
+        if ($user->kode_bagian == 'k45') {
+            $patients = collect($this->getPatientData());
+        } else {
+            $patients = collect($this->getPatientData())->where('ServiceUnitName', $serviceUnit)
+                ->filter(function ($patient) use ($serviceUnit) {
+                    return $patient->ServiceUnitName === $serviceUnit;
+                });
+        }
+
+        $currentTime = Carbon::now();
+
+        foreach ($patients as $patient) {
+            // Patient's short note.
+            $patient->short_note = $patient->CatRencanaPulang ? Str::limit($patient->CatRencanaPulang, 10) : '-';
+
+            // Mengambil waktu rencana pulang
+            $dischargeTime = Carbon::parse($patient->RencanaPulang);
+
+            // Menghitung waktu tunggu
+            if ($dischargeTime->gt($currentTime)) {
+                // Jika waktu rencana pulang di masa depan
+                $waitTime = '00:00:00'; // Waktu tunggu belum dimulai
+                $waitTimeInSeconds = 0; // Inisialisasi waitTimeInSeconds sebagai 0.
             } else {
-                $patients = collect($this->getPatientData())->where('ServiceUnitName', $serviceUnit)
-                            ->filter(function ($patient) use ($serviceUnit) {
-                                return $patient->ServiceUnitName === $serviceUnit;
-                            });
+                // Menghitung selisih waktu
+                $waitTimeInSeconds = $dischargeTime->diffInSeconds($currentTime);
+
+                // Format waktu tunggu dalam format hh:mm:ss
+                $hours = floor($waitTimeInSeconds / 3600);
+                $minutes = floor(($waitTimeInSeconds % 3600) / 60);
+                $seconds = $waitTimeInSeconds % 60;
+                $waitTime = sprintf('%02d:%02d:%02d', $hours, $minutes, $seconds);
+            }
+            $patient->wait_time = $waitTime;
+
+            $standardWaitTimeInSeconds = 7200; // 2 hours
+            if ($patient->Keterangan == 'Tunggu Obat Farmasi') {
+                $standardWaitTimeInSeconds = 3600; // 1 hour
+            } else if ($patient->Keterangan == 'Penyelesaian Administrasi Pasien (Billing)') {
+                $standardWaitTimeInSeconds = 900; // 15 minutes
             }
 
-            $currentTime = Carbon::now();
-
-            foreach ($patients as $patient) {
-                // Patient's short note.
-                $patient->short_note = $patient->CatRencanaPulang ? Str::limit($patient->CatRencanaPulang, 10) : '-';
-
-                // Mengambil waktu rencana pulang
-                $dischargeTime = Carbon::parse($patient->RencanaPulang);
-
-                // Menghitung waktu tunggu
-                if ($dischargeTime->gt($currentTime)) {
-                    // Jika waktu rencana pulang di masa depan
-                    $waitTime = '00:00:00'; // Waktu tunggu belum dimulai
-                    $waitTimeInSeconds = 0; // Inisialisasi waitTimeInSeconds sebagai 0.
-                } else {
-                    // Menghitung selisih waktu
-                    $waitTimeInSeconds = $dischargeTime->diffInSeconds($currentTime);
-
-                    // Format waktu tunggu dalam format hh:mm:ss
-                    $hours = floor($waitTimeInSeconds / 3600);
-                    $minutes = floor(($waitTimeInSeconds % 3600) / 60);
-                    $seconds = $waitTimeInSeconds % 60;
-                    $waitTime = sprintf('%02d:%02d:%02d', $hours, $minutes, $seconds);
-                }    
-                $patient->wait_time = $waitTime;
-
-                $standardWaitTimeInSeconds = 7200; // 2 hours
-                if ($patient->Keterangan == 'Tunggu Obat Farmasi') {
-                    $standardWaitTimeInSeconds = 3600; // 1 hour
-                } else if ($patient->Keterangan == 'Penyelesaian Administrasi Pasien (Billing)') {
-                    $standardWaitTimeInSeconds = 900; // 15 minutes
-                }
-
-                $progressPercentage = min(($waitTimeInSeconds / $standardWaitTimeInSeconds) * 100, 100);
-                $patient->progress_percentage = $progressPercentage;
-            }
+            $progressPercentage = min(($waitTimeInSeconds / $standardWaitTimeInSeconds) * 100, 100);
+            $patient->progress_percentage = $progressPercentage;
+        }
 
         /* MEMBUAT URUTAN UNTUK TAMPILAN KOLOM KETERANGAN.*/
-            // Mapping data ke dalam array untuk diteruskan ke view
-            $groupedData = [];
-            foreach ($patients as $data) {
-                $groupedData[$data->Keterangan][] = $data;
+        // Mapping data ke dalam array untuk diteruskan ke view
+        $groupedData = [];
+        foreach ($patients as $data) {
+            $groupedData[$data->Keterangan][] = $data;
+        }
+
+        // Urutan kustom untuk 'keterangan'
+        $order = [
+            'Tunggu Jangdik',
+            'Tunggu Keperawatan',
+            'Tunggu Farmasi',
+            'Tunggu Kasir',
+            'Selesai Kasir'
+        ];
+
+        // Ambil data yang sudah dikelompokkan (groupedData)
+        $groupedData = collect($groupedData)->sortBy(function ($patients, $keterangan) use ($order) {
+            return array_search($keterangan, $order);
+        })->toArray();
+
+        $allPatients = [];
+        foreach ($groupedData as $patients) {
+            foreach ($patients as $patient) {
+                $allPatients[] = $patient;
             }
-
-            // Urutan kustom untuk 'keterangan'
-            $order = [
-                'Tunggu Jangdik',
-                'Tunggu Keperawatan',
-                'Tunggu Farmasi',
-                'Tunggu Kasir',
-                'Selesai Kasir'
-            ];
-
-            // Ambil data yang sudah dikelompokkan (groupedData)
-            $groupedData = collect($groupedData)->sortBy(function($patients, $keterangan) use ($order) {
-                return array_search($keterangan, $order);
-            })->toArray();
-
-            $allPatients = [];
-            foreach ($groupedData as $patients) {
-                foreach ($patients as $patient) {
-                    $allPatients[] = $patient;
-                }
-            }
+        }
 
         /* WARNA HEADER KARTU BERDASARKAN customerType (PENJAMIN BAYAR). */
         $customerTypeColors = [
@@ -215,7 +216,7 @@ class RanapController extends Controller
         //dd($patients);
 
         /* MENGIRIM DATA KE VIEW. */
-        return view('Ranap.ranap', compact('groupedData', 'allPatients','customerTypeColors'));
+        return view('Ranap.ranap', compact('groupedData', 'allPatients', 'customerTypeColors'));
     }
 
     // Fungsi untuk mendapatkan ServiceUnitName berdasarkan kode_bagian
